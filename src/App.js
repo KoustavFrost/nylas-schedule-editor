8c8495c98ee63a0fbb299f36357fef5f303587c3
Koustav <kv.pal9@gmail.com>
2024-09-27 16:12:03 +0530

    Added nylas code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { NylasSessions } from '@nylas/identity';
+import { NylasIdentityRequestWrapper } from '@nylas/react';
 
 function App() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+  const configId = urlParams.get('config_id') || '';
+  const componentSettings = {
+    authSettings: {
+      scopes: {
+        google: [
+          'openid',
+          'https://www.googleapis.com/auth/userinfo.email',
+          'https://www.googleapis.com/auth/userinfo.profile',
+          'https://www.googleapis.com/auth/contacts',
+          'https://www.googleapis.com/auth/calendar',
+          'https://www.googleapis.com/auth/directory.readonly'
+        ],
+        microsoft: [
+          'Calendars.ReadWrite',
+          'Mail.ReadWrite',
+          'Contacts.ReadWrite',
+          'User.Read',
+          'offline_access'
+        ]
+      }
+    }
+  };
+
+  const identitySettings = {
+    clientId: clientId,
+    redirectUri: `${window.location.origin}/nylas-auth/scheduler-editor`,
+    domain: 'https://api.us.nylas.com/v3',
+    hosted: true,
+    accessType: 'offline'
+  };
+  const identity = new NylasSessions(identitySettings);
+  const nylasApiRequest = new NylasIdentityRequestWrapper(identity);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/hello'
+          element={
+            <div>
+              <h2>Hello world</h2>
+            </div>
+          }
+        ></Route>
+        <Route
+          path='/nylas-auth/scheduler-editor'
+          element={<div>Hi</div>}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
